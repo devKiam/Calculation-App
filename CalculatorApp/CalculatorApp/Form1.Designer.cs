ad1bf6120ef49b7f188853185663e4da0967ea4e
Md. Nuho-Ul Alam Kiam <mdnuhoulalam@gmail.com>
2021-03-26 03:02:36 +0600

    Add files via upload

---
+++ CalculatorApp/CalculatorApp/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace CalculatorApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.addButton = new System.Windows.Forms.Button();
+            this.subtractionButton = new System.Windows.Forms.Button();
+            this.multiplicationButton = new System.Windows.Forms.Button();
+            this.divisioinButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.resultBox = new System.Windows.Forms.TextBox();
+            this.allCalculatioinsButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(112, 117);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "First Number";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(112, 157);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Second Number";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(112, 254);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 2;
+            this.addButton.Text = "+";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // subtractionButton
+            // 
+            this.subtractionButton.Location = new System.Drawing.Point(247, 254);
+            this.subtractionButton.Name = "subtractionButton";
+            this.subtractionButton.Size = new System.Drawing.Size(75, 23);
+            this.subtractionButton.TabIndex = 3;
+            this.subtractionButton.Text = "-";
+            this.subtractionButton.UseVisualStyleBackColor = true;
+            this.subtractionButton.Click += new System.EventHandler(this.subtractionButton_Click);
+            // 
+            // multiplicationButton
+            // 
+            this.multiplicationButton.Location = new System.Drawing.Point(112, 310);
+            this.multiplicationButton.Name = "multiplicationButton";
+            this.multiplicationButton.Size = new System.Drawing.Size(75, 23);
+            this.multiplicationButton.TabIndex = 4;
+            this.multiplicationButton.Text = "x";
+            this.multiplicationButton.UseVisualStyleBackColor = true;
+            this.multiplicationButton.Click += new System.EventHandler(this.multiplicationButton_Click);
+            // 
+            // divisioinButton
+            // 
+            this.divisioinButton.Location = new System.Drawing.Point(247, 310);
+            this.divisioinButton.Name = "divisioinButton";
+            this.divisioinButton.Size = new System.Drawing.Size(75, 23);
+            this.divisioinButton.TabIndex = 5;
+            this.divisioinButton.Text = "/";
+            this.divisioinButton.UseVisualStyleBackColor = true;
+            this.divisioinButton.Click += new System.EventHandler(this.divisioinButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(247, 117);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(247, 157);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // resultBox
+            // 
+            this.resultBox.Location = new System.Drawing.Point(523, 117);
+            this.resultBox.Multiline = true;
+            this.resultBox.Name = "resultBox";
+            this.resultBox.Size = new System.Drawing.Size(212, 216);
+            this.resultBox.TabIndex = 8;
+            // 
+            // allCalculatioinsButton
+            // 
+            this.allCalculatioinsButton.Location = new System.Drawing.Point(394, 283);
+            this.allCalculatioinsButton.Name = "allCalculatioinsButton";
+            this.allCalculatioinsButton.Size = new System.Drawing.Size(94, 23);
+            this.allCalculatioinsButton.TabIndex = 9;
+            this.allCalculatioinsButton.Text = "All calculations";
+            this.allCalculatioinsButton.UseVisualStyleBackColor = true;
+            this.allCalculatioinsButton.Click += new System.EventHandler(this.allCalculatioinsButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.allCalculatioinsButton);
+            this.Controls.Add(this.resultBox);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.divisioinButton);
+            this.Controls.Add(this.multiplicationButton);
+            this.Controls.Add(this.subtractionButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Button allCalculatioinsButton;
+
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox resultBox;
+
+        private System.Windows.Forms.TextBox textBox1;
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button subtractionButton;
+        private System.Windows.Forms.Button multiplicationButton;
+        private System.Windows.Forms.Button divisioinButton;
+
+        private System.Windows.Forms.Button button1;
+
+        #endregion
+    }
+}
---
+++ CalculatorApp/CalculatorApp/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //throw new System.NotImplementedException();
+        }
+
+
+        private double GetNum1()
+        {
+            return double.Parse(textBox1.Text);
+        }
+
+        
+        private double GetNum2()
+        {
+            return double.Parse(textBox2.Text);
+        }
+        
+        
+        private double Additon(double num1, double num2)
+        {
+            double result = num1 + num2;
+            return result;
+        }
+
+        
+        private double Subtraction(double num1, double num2)
+        {
+            double result = num1 - num2;
+            return result;
+        }
+        
+        
+        private double Multiplication(double num1, double num2)
+        {
+            double result = num1 * num2;
+            return result;
+        }
+        
+        
+        private double Division(double num1, double num2)
+        {
+            double result = num1 / num2;
+            return result;
+        }
+        
+        
+        
+        // Events functions.....................................
+        
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            resultBox.Text = $"The result of addition is: {Additon(GetNum1(), GetNum2())}";
+        }
+
+
+        private void subtractionButton_Click(object sender, EventArgs e)
+        {
+            resultBox.Text = $"The result of subtraction is: {Subtraction(GetNum1(), GetNum2())}";
+        }
+
+        private void multiplicationButton_Click(object sender, EventArgs e)
+        {
+            resultBox.Text = $"The result of multiplication is: {Multiplication(GetNum1(), GetNum2())}";
+        }
+
+        private void divisioinButton_Click(object sender, EventArgs e)
+        {
+            resultBox.Text = $"The result of division is: {Division(GetNum1(), GetNum2())}";
+        }
+
+        private void allCalculatioinsButton_Click(object sender, EventArgs e)
+        {
+            resultBox.Text = ($"The result of addition is: {Additon(GetNum1(), GetNum2())}\r\n" +
+                              $"The result of subtraction is: {Subtraction(GetNum1(), GetNum2())}\r\n" +
+                              $"The result of multiplication is: {Multiplication(GetNum1(), GetNum2())}\r\n" +
+                              $"The result of division is: {Division(GetNum1(), GetNum2())}");
+        }
+    }
+}
